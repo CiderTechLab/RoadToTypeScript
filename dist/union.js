f6d3e8ad7ad5360fe33aa855830a282c02884129
SK <SK@example.com>
2026-01-29 22:00:13 +0900

    update: union.ts

--- dist/union.js
+++ dist/union.js
@@ -1,28 +1,32 @@
 /**
- * This funtion is for studying union types.
- * @param os {"windows", "linux", "macos"}
+ * value is OS: 型述語（Type Predicate）
+ * → 「この関数が true を返したとき、value は OS 型であると見なしてよい」
+ * @param value
+ * @returns {OS}
  */
-import * as readline from "readline";
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+function isOS(value) {
+    /** 実行時チェック */
+    return value === "windows" || value === "linux" || value === "macos";
+}
 function selectOS(useOS) {
     let comment;
     switch (useOS) {
         case "windows":
-            comment = "windows?他のOSが良いですよ！";
+            comment = "windows?他のOSが良いですよ!";
             break;
         case "linux":
             comment = "linux!素晴らしい選択です！";
             break;
         case "macos":
-            comment = "macosを使っているんですね！おしゃれです！";
-            console.log(comment);
+            comment = "macosを使っているんですね!おしゃれです!";
             break;
         default:
             comment = "知らないOSですね。";
     }
-    return comment;
+    console.log(comment);
 }
-selectOS();
+const answer = "masos";
+if (isOS(answer)) {
+    selectOS(answer);
+}
+export {};
--- src/union.ts
+++ src/union.ts
@@ -1,33 +1,46 @@
 /**
- * This funtion is for studying union types.
+ * This funtion is for studying union types
+ * and readline module.
  * @param os {"windows", "linux", "macos"}
  */
 import * as readline from "readline";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+type OS = "windows" | "linux" | "macos";
 
-function selectOS(useOS: "windows" | "linux" | "macos"): string {
+/**
+ * value is OS: 型述語（Type Predicate）
+ * → 「この関数が true を返したとき、value は OS 型であると見なしてよい」
+ * @param value 
+ * @returns {OS}
+ */
+function isOS(value:String):value is OS {
+  /** 実行時チェック */
+  return value === "windows" || value === "linux" || value === "macos";
+}
+
+function selectOS(useOS: "windows" | "linux" | "macos") {
   let comment: string;
 
   switch (useOS) {
     case "windows":
-      comment = "windows?他のOSが良いですよ！";
+      comment = "windows?他のOSが良いですよ!";
       break;
     case "linux":
       comment = "linux!素晴らしい選択です！";
       break;
     case "macos":
-      comment = "macosを使っているんですね！おしゃれです！";
-      console.log(comment);
+      comment = "macosを使っているんですね!おしゃれです!";
       break;
     default:
       comment = "知らないOSですね。";
   }
+  
+  console.log(comment);
 
-  return comment;
 }
 
-selectOS(rl);
+const answer = "masos";
+
+if (isOS(answer)) {
+  selectOS(answer);
+}
